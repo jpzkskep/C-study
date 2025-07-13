308e7689075dd0fb68f4b39c2082936a34b50207
jpzkskep <jpzkskep@gmail.com>
2025-07-13 11:48:57 +0900

    7/13

---
+++ 7-13/Project2/소스.c
@@ -0,0 +1,23 @@
+//동적할당 활용
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char** argv) {
+	int student;	//입력 받고자 하는 학생 수
+	int i, input;
+	int* score;	//학생들의 수학점수 변수
+	int sum = 0;
+	printf("학생수는 ? ");
+	scanf("%d", &student);
+	score = (int*)malloc(student * sizeof(int));
+	for (i = 0; i < student; i++) {
+		printf("학생%d의 점수 : ", i);
+		scanf("%d", &input);
+		score[i] = input;
+	}
+	for (i = 0; i < student; i++) {
+		sum += score[i];
+	}
+	printf("전체 학생 평균 점수 : %d \n", sum / student);
+	free(score);
+	return 0;
+}
