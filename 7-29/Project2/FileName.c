754b404dea54ea2c106c0b717db265708c289365
jpzkskep <jpzkskep@gmail.com>
2025-07-29 08:30:08 +0900

    7/29

---
+++ 7-29/Project2/FileName.c
@@ -0,0 +1,10 @@
+//문자열 복사
+#include <stdio.h>
+#include <string.h>
+int main() {
+	char str1[20] = { "hi" };
+	char str2[20] = { "hello every1" };
+	strcpy(str1, str2);
+	printf("str1 : %s\n",str1);
+	return 0;
+}
