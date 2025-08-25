b5619abfedab47d004ead967c35d9793e4f1a3c2
jpzkskep <jpzkskep@gmail.com>
2025-08-25 09:45:54 +0900

    8/25

---
+++ 열혈c프로그래밍/8월/8-23-8-25/8-25/Project2/stdiv.h
@@ -0,0 +1,4 @@
+typedef struct div {
+	int quotient;	//¸ò
+	int remainder;	//³ª¸ÓÁö
+}Div;
---
+++ 열혈c프로그래밍/8월/8-23-8-25/8-25/Project3/stdiv2.h
@@ -0,0 +1,7 @@
+#ifndef __STDIV2_H__
+#define __STDIV2_H__
+typedef struct div {
+	int quotient;
+	int remainder;
+} Div;
+#endif
